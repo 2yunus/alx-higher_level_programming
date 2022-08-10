56da730e621cab8c10e98d4eb25bc8b4795dafaa
yunus mohammed <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-08-10 11:54:11 +0300

    Update 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Script that prints all with same score
 -- query that prints all by grouping by score
-SELECT score COUNT(score) as number FROM  second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM  second_table GROUP BY score ORDER BY score DESC;
