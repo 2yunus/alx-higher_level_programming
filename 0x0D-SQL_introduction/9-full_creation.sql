ed55c4fe66c10084266b0213178069a5c2d73202
yunus mohammed <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-08-10 09:39:02 +0300

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- CREATE ANOTHER TABLE WITH ID,NAME,SCORE
 -- Query that creates table
-CREATE TABLE if not exists second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 id INT,
 name VARCHAR(256),
 score INT);
