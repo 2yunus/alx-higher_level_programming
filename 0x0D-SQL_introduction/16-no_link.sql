38cbbb6c69fe2dc8bcee0216cd67a77e521239db
yunus mohammed <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-08-10 11:56:36 +0300

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- Script that prints all the records of the second table
 -- query all the records without the names which doesn't have name
-SELECT score, name FROM second_table WHERE name NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
