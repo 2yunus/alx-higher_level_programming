309fa08251f97cfad72626954b37bf6af3584465
yunus mohammed <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-08-10 10:40:00 +0300

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- displays the score then the name
 -- query that display all the records
-select score, name  from second_table order by score desc;
+SELECT score, name FROM second_table ORDER BY score DESC;
