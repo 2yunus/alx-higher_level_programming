e795e678cd32430dbbe063d889368df1d2534dbb
yunus mohammed <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-08-10 09:38:10 +0300

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,7 +3,7 @@
 CREATE TABLE if not exists second_table (
 id INT,
 name VARCHAR(256),
-SCORE INT);
+score INT);
 INSERT INTO second_table VALUES(1,'John',10)
 INSERT INTO second_table VALUES(2,'Alex',3)
 INSERT INTO second_table VALUES(3,'Bob',14)
