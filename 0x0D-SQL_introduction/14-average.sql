72afa015a7f6df15163e0cb35543285fe609b29a
yunus mohammed <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-08-10 11:19:41 +0300

    Update 14-average.sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Script that calclates the average of the columns
 -- query that prins the avaerage of score
-SELECT AVG(score) 'averege' FROM second_table;
+SELECT AVG(score) AS 'averege' FROM second_table;
