32b3d43a4ce68959e99f6899d7ac108f5353728c
yunus mohammed <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-08-10 10:45:44 +0300

    Update 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- That prints score >= 10
 -- Query that displays all records that is greater than 10
-SELECT score, name FROM second_table where score >= 10; 
+SELECT score, name FROM second_table where score >= 10 OREDER BY DESC; 
